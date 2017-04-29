0e371bc4393f99bede8d9e0f7d2833137703d50f
Kyle Ziegler <kziegler17@gmail.com>
2017-04-29 10:30:05 -0700

    Updated the Delete and Select queries

--- sql/Categories_DELETE.sql
+++ sql/Categories_DELETE.sql
@@ -3,5 +3,6 @@
 
 /*Incomplete, need to do an inner join on categories_products and see if there are 
 any products associated to this category*/
-DELETE FROM public.categories
-	WHERE "Name" = 'NameToDelete';
+/*Replace the 6 with the id of the category that you are trying to delete*/
+Delete from categories 
+
--- sql/Categories_SELECT.sql
+++ sql/Categories_SELECT.sql
@@ -1,2 +1,3 @@
 /*Category Select*/
+/*Loads categories associated to a user account*/
 Select * from Categories where AccountID = 1; 
