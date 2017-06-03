1888cbba8b8b310673fc59e72bdd1b7f8ab9d9f3
Kyle Ziegler <kziegler17@gmail.com>
2017-06-02 17:11:22 -0700

    Added the query for similar products

--- sql/SalesAnalytics.sql
+++ sql/SalesAnalytics.sql
@@ -19,88 +19,29 @@ Limit 10;
 
 --Start regular grid queries
 
---User rows, Alphabetical, init
-SELECT a."Username", p."ProductID", ohp."Price"
-FROM products p, order_history_products ohp, accounts a, order_history oh
-WHERE a."AccountID" = oh."AccountID"
-AND
-ohp."OrderHistoryID" = oh."OrderHistoryID"
-AND
-ohp."ProductID" = p."ProductID"
-Group by a."Username", p."ProductID", ohp."Price"
-Order by "Username" asc, p."Name" asc
-LIMIT 20;
+SELECT producttable."Username" as header, producttable."Name", producttable."ProductID", coalesce(SUM(orders."Price"), 0) as productprice, producttable.totalprice as totalprice
+FROM (Select * FROM (
+SELECT a."Username", a."AccountID", coalesce(SUM(CAST(oh."TotalPrice" as bigint)), 0) as TotalPrice
+FROM accounts a 
+LEFT OUTER JOIN order_history oh 
+ON a."AccountID" = oh."AccountID"
+GROUP BY a."Username", a."AccountID"
+ORDER BY a."Username"
+LIMIT 20 OFFSET ?) a CROSS JOIN (
+SELECT "ProductID", p."Name"
+FROM products p, categories c
+WHERE p."CategoryID" = c."CategoryID"
+AND c."Name" = ?
+ORDER BY p."Name"
+LIMIT 10 OFFSET ?) product) producttable
+LEFT OUTER JOIN (SELECT oh."AccountID", ohp."ProductID", CAST(ohp."Price" as bigint)
+FROM order_history_products ohp, order_history oh
+WHERE ohp."OrderHistoryID" = oh."OrderHistoryID"
+) orders
+ON orders."ProductID" = producttable."ProductID"
+AND orders."AccountID" = producttable."AccountID"
+GROUP BY producttable."Username", producttable.totalprice, producttable."Name", producttable."ProductID"
+ORDER BY header, totalprice, producttable."Name", productprice
 
 
---latest from kyle
---State rows, Alphabetical, init
-SELECT a."State", p."ProductID", ohp."Price" as totalSales
-FROM products p, order_history_products ohp, accounts a, order_history oh
-WHERE a."AccountID" = oh."AccountID"
-AND
-ohp."OrderHistoryID" = oh."OrderHistoryID"
-AND
-ohp."ProductID"  = p."ProductID"
-AND p."ProductID" in (Select "ProductID" from products order by "Name" asc limit 10)
-Group by a."State", p."ProductID", ohp."Price"
-Order by "State" asc, p."Name" asc
-LIMIT 20;
-
---john updated
-Select *
-FROM (SELECT ROW_NUMBER() OVER (partition by a."State" ORDER BY p."Name") as r, a."State", p."ProductID", ohp."Price"
-FROM products p, order_history_products ohp, accounts a, order_history oh
-WHERE a."AccountID" = oh."AccountID"
-AND
-ohp."OrderHistoryID" = oh."OrderHistoryID"
-AND
-ohp."ProductID" = p."ProductID"
-Group by a."State", p."ProductID", ohp."Price"
-Order by "State" asc, p."Name"
-) x
-WHERE x.r <= 10;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---State rows, Alphabetical, Category filter
-SELECT a."State", a."AccountID", ohp."Price"
-FROM products p, order_history_products ohp, accounts a, order_history oh
-WHERE a."AccountID" = oh."AccountID"
-AND
-ohp."OrderHistoryID" = oh."OrderHistoryID"
-AND
-ohp."ProductID" = p."ProductID"
-AND
-p."ProductID" = ?
-Group by a."State", a."AccountID", ohp."Price"
-Order by "State" asc
-LIMIT 20;
-
-
-
---Top K --Working on these
-
---Close, but right now they are grouped by the top item sales in each state and
---not the entire state sales
---Top K states
-SELECT a."State", p."ProductID", SUM(CAST(ohp."Price" AS bigint)) as totalStateSales
-FROM products p, order_history_products ohp, accounts a, order_history oh
-WHERE a."AccountID" = oh."AccountID"
-AND
-ohp."OrderHistoryID" = oh."OrderHistoryID"
-AND
-ohp."ProductID" = p."ProductID"
-Group by a."State", p."ProductID"
-Order by totalStateSales desc
-LIMIT 20;
+--Indexes that will benifit: account Username, product name, category name, 
